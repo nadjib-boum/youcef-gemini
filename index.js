c9be6981d149b016a1febd9593470e05e7b38f1e
nadjib-boum <nadjib.dev22@gmail.com>
2025-05-10 23:44:28 +0400

    unlink file after finish

--- index.js
+++ index.js
@@ -21,8 +21,14 @@ const ai = new GoogleGenAI({
 
 // POST endpoint to upload a file
 app.post("/upload", upload.single("file"), async (req, res) => {
+
+  if (!req.file) {
+    return res.status(400).json({ success: false, error:{ message: "No file uploaded" } });
+  }
+
+  const filePath = req.file.path;
+
   try {
-    const filePath = req.file.path;
 
     // First Step: Upload the file to Google GenAI
     const uploadedFile = await ai.files.upload({
@@ -34,12 +40,9 @@ app.post("/upload", upload.single("file"), async (req, res) => {
       model: "gemini-1.5-flash",
       contents: createUserContent([
         createPartFromUri(uploadedFile.uri, uploadedFile.mimeType),
-        `extract the date from this image in this format dd/mm/yyyy
-I want raw date value without additions or changes`,
+        `extract the date from this image in this format dd/mm/yyyy. I want raw date value without additions or changes`,
       ]),
     });
-
-    fs.unlinkSync(filePath);
 
     res.status(200).json({
       success: true,
@@ -50,44 +53,20 @@ I want raw date value without additions or changes`,
     });
 
   } catch (error) {
+
     console.error("Upload failed:", error);
     res.status(500).json({ success: false, message: "Upload failed", error });
+
+  } finally {
+
+    await fs.unlink(filePath)
+
   }
+
+
+
 });
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
-
-
-
-// import {
-//   GoogleGenAI,
-//   createUserContent,
-//   createPartFromUri,
-// } from "@google/genai";
-// import dotenv from "dotenv";
-// import express from "express";
-// // import cors from "cors";
-
-// dotenv.config();
-
-// const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_API_KEY });
-
-// async function main() {
-//   const myfile = await ai.files.upload({
-//     file: "path/to/sample.jpg",
-//     config: { mimeType: "image/jpeg" },
-//   });
-
-//   const response = await ai.models.generateContent({
-//     model: "gemini-2.0-flash",
-//     contents: createUserContent([
-//       createPartFromUri(myfile.uri, myfile.mimeType),
-//       "Caption this image.",
-//     ]),
-//   });
-//   console.log(response.text);
-// }
-
-// await main();
